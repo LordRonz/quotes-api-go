0d4bb81c0d944e777088f3001bec5ba6a02c2387
LordRonz <aarontanhar2000@gmail.com>
2022-10-17 12:33:09 +0700

    test: more unit test :rocket:

--- cmd/utils/get_env_test.go
+++ cmd/utils/get_env_test.go
@@ -1,10 +1,23 @@
 package utils
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestGetEnv(t *testing.T) {
 	got := GetEnv("NotExistingEnv", "default")
 	want := "default"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestSetGetEnv(t *testing.T) {
+	os.Setenv("Balls", "BIG")
+	got := GetEnv("Balls", "SMOL")
+	want := "BIG"
 
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
